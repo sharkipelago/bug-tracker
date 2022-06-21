daa708e59a2f5cb2f79aa44f8ea5d67ab06b09a4
baozi <gchozick@gmail.com>
2022-06-21 11:50:10 -0400

    created database logic and schema

---
+++ bug-star/schema.sql
@@ -0,0 +1,26 @@
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS issue;
+
+CREATE TABLE user (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  username TEXT UNIQUE NOT NULL,
+  password TEXT NOT NULL
+);
+
+CREATE TABLE issue (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  author_id INTEGER NOT NULL,
+  closer_id INTEGER, --user who closes issue
+  created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  title TEXT NOT NULL,
+  body TEXT NOT NULL,
+  FOREIGN KEY (author_id) REFERENCES user (id)
+);
+
+CREATE TABLE assignees (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    issue_id INTEGER NOT NULL,
+    assignee_id INTEGER NOT NULL,
+    FOREIGN KEY (issue_id) REFERENCES issue (id),
+    FOREIGN KEY (assignee_id) REFERENCES user (id)
+)
