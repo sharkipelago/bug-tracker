8535d11fa7fa8c2ac48909e9dcea10bc322f4bef
baozi <gchozick@gmail.com>
2022-06-23 20:28:39 -0400

    added /base route for testing

--- bugstar/auth.py
+++ bugstar/auth.py
@@ -89,3 +89,8 @@ def login_required(view):
         return view(**kwargs)
 
     return wrapped_view
+
+#Remove this after editing template
+@bp.route('/base')
+def based():
+    return render_template('base.html')
