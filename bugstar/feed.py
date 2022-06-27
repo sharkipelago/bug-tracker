a1173f85dd0b895c15c28b438d2ede74d807a057
baozi <gchozick@gmail.com>
2022-06-27 17:02:06 -0400

    implemented creating and displaying issues

--- bugstar/feed.py
+++ bugstar/feed.py
@@ -13,7 +13,7 @@ bp = Blueprint('feed', __name__)
 def index():
     db = get_db()
     issues = db.execute(
-        'SELECT i.id, author_id, closer_id, created, title, body'
+        'SELECT i.id, author_id, closer_id, created, title, body, (firstname || " " || lastname) AS authorname'
         ' FROM issue i JOIN user u ON i.author_id = u.id'
         ' ORDER BY created DESC'
     ).fetchall()
@@ -35,35 +35,35 @@ def create():
         else:
             db = get_db()
             db.execute(
-                'INSERT INTO post (title, body, author_id)'
+                'INSERT INTO issue (title, body, author_id)'
                 ' VALUES (?, ?, ?)',
                 (title, body, g.user['id'])
             )
             db.commit()
-            return redirect(url_for('feed.index'))
+            return redirect(url_for('index'))
 
-    return render_template('feed/create.html')
+    return render_template('feed/create.html', user=g.user)
 
-def get_post(id, check_author=True):
-    post = get_db().execute(
+def get_issue(id, check_author=True):
+    issue = get_db().execute(
         'SELECT p.id, title, body, created, author_id, username'
-        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' FROM issue p JOIN user u ON p.author_id = u.id'
         ' WHERE p.id = ?',
         (id,)
     ).fetchone()
 
-    if post is None:
-        abort(404, f"Post id {id} doesn't exist.")
+    if issue is None:
+        abort(404, f"issue id {id} doesn't exist.")
 
-    if check_author and post['author_id'] != g.user['id']:
+    if check_author and issue['author_id'] != g.user['id']:
         abort(403)
 
-    return post
+    return issue
 
 @bp.route('/<int:id>/update', methods=('GET', 'POST'))
 @login_required
 def update(id):
-    post = get_post(id)
+    issue = get_issue(id)
 
     if request.method == 'POST':
         title = request.form['title']
@@ -78,20 +78,20 @@ def update(id):
         else:
             db = get_db()
             db.execute(
-                'UPDATE post SET title = ?, body = ?'
+                'UPDATE issue SET title = ?, body = ?'
                 ' WHERE id = ?',
                 (title, body, id)
             )
             db.commit()
             return redirect(url_for('feed.index'))
 
-    return render_template('feed/update.html', post=post)
+    return render_template('feed/update.html', issue=issue)
 
 @bp.route('/<int:id>/delete', methods=('POST',))
 @login_required
 def delete(id):
-    get_post(id)
+    get_issue(id)
     db = get_db()
-    db.execute('DELETE FROM post WHERE id = ?', (id,))
+    db.execute('DELETE FROM issue WHERE id = ?', (id,))
     db.commit()
     return redirect(url_for('feed.index'))
