df6d585f24e78f0c30212da0121b97176e9331a2
baozi <gchozick@gmail.com>
2022-06-26 20:33:03 -0400

    changed SQL query to match current project instead of flask tutorial

--- bugstar/feed.py
+++ bugstar/feed.py
@@ -13,8 +13,8 @@ bp = Blueprint('feed', __name__)
 def index():
     db = get_db()
     posts = db.execute(
-        'SELECT p.id, title, body, created, author_id, username'
-        ' FROM post p JOIN user u ON p.author_id = u.id'
+        'SELECT i.id, author_id, closer_id, created, title, body'
+        ' FROM issue i JOIN user u ON i.author_id = u.id'
         ' ORDER BY created DESC'
     ).fetchall()
     return render_template('feed/index.html', posts=posts)
