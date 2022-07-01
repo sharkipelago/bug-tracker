f59d0c321a6470b1ba367baf2bf31edd0b23dd8c
baozi <gchozick@gmail.com>
2022-07-01 17:48:12 -0400

    implemented assignees in creating/displaying issues

--- bugstar/feed.py
+++ bugstar/feed.py
@@ -12,19 +12,47 @@ bp = Blueprint('feed', __name__)
 @login_required
 def index():
     db = get_db()
+
     issues = db.execute(
-        'SELECT i.id, author_id, closer_id, created, title, body, (firstname || " " || lastname) AS authorname'
-        ' FROM issue i JOIN user u ON i.author_id = u.id'
-        ' ORDER BY created DESC'
+        f'{get_index_db_statement()}'
     ).fetchall()
     return render_template('feed/index.html', issues=issues, user=g.user)
+
+def get_index_db_statement():
+    # Converts Assignee ids into names
+    assignee_names = (
+        'SELECT issue_id, (firstname || " " || lastname) AS name'
+        ' FROM assignee a'
+        ' JOIN user u ON a.assignee_id = u.id'
+    )
+
+    # Group assignee names by issue they are related to
+    issue_assignees = (
+        'SELECT GROUP_CONCAT(name) assignees, closer_id, created, title, body, author_id'
+        ' FROM issue i'
+        f' LEFT JOIN ({assignee_names}) a ON a.issue_id = i.id'
+        ' GROUP BY i.id'
+    )
+
+    db_statement = (
+        'SELECT closer_id, created, title, body, assignees,'
+        ' (firstname || " " || lastname) AS authorname'
+        f' FROM ({issue_assignees}) i'
+        ' LEFT JOIN user u ON i.author_id = u.id'
+        ' ORDER BY created DESC'
+    )
+
+    return db_statement
 
 @bp.route('/create', methods=('GET', 'POST'))
 @login_required
 def create():
+    db = get_db()
     if request.method == 'POST':
         title = request.form['title']
         body = request.form['body']
+        assignees =  request.form.getlist('assignees')
+        
         error = None
 
         if not title:
@@ -33,16 +61,35 @@ def create():
         if error is not None:
             flash(error)
         else:
-            db = get_db()
             db.execute(
                 'INSERT INTO issue (title, body, author_id)'
                 ' VALUES (?, ?, ?)',
                 (title, body, g.user['id'])
             )
             db.commit()
+            issue_id = db.execute(
+                'SELECT i.id'
+                ' FROM issue i'
+                ' WHERE title = ?'
+                ' ORDER BY created DESC',
+                (title, )
+            ).fetchone()['id']
+            for assignee in assignees:
+                db.execute(
+                    'INSERT INTO assignee (issue_id, assignee_id)'
+                    ' VALUES (?, ?)',
+                    (issue_id, assignee)
+                )
+            db.commit()
             return redirect(url_for('index'))
 
-    return render_template('feed/create.html', user=g.user)
+    all_users = db.execute(
+        'SELECT id, (firstname || " " || lastname) AS name'
+        ' FROM user'
+        ' ORDER BY lastname'
+    ).fetchall()
+
+    return render_template('feed/create.html', user=g.user, all_users=all_users)
 
 def get_issue(id, check_author=True):
     issue = get_db().execute(
